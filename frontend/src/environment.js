85cd233f77c94795b2424e0c995f9fdee2deb97d
Ayush Yadav <ayush139yadav@gmail.com>
2026-07-05 11:26:33 +0530

    fix url problem

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,7 @@
 let IS_PROD = true;
 
 const server = IS_PROD
-    ? "https://visionmeetbackend-me53.onrender.com/"
+    ? "https://visionmeetbackend-me53.onrender.com"
     : "http://localhost:8000";
 
-export default server;
+export default server;g
